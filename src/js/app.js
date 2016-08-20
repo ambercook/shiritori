cc91a1a94c836b6636ad9d6ddbb73803c631c0b4
Amber <amber.l.cook@gmail.com>
2016-08-20 14:32:56 -0400

    playing around with bulma to create a basic layout

--- src/js/app.js
+++ src/js/app.js
@@ -2,19 +2,77 @@ require("../scss/style.scss");
 import React, {Component} from 'react';
 import Navigation from './components/Navigation';
 
+
 class App extends Component {
   render() {
     return (
 
-    	<div class="container">
-		      
+    	<div>
 
 	    	<header className="header">
-		    	<h1 class="title">Shiritori</h1>
-		    	<Navigation></Navigation>
-		    </header>
+
+	    		<div className="container">
+
+	    			<div className="columns">
+
+	    				<div className="column is-two-thirds">
+		    				<h1 className="title is-1">Shiritori</h1>
+		    			</div>
+
+		    			<div className="column">
+		    				<Navigation></Navigation>
+		    			</div>
+
+		    		</div>
+
+
+		    	</div>
+
+			</header>
+
+			<section className="section">
+
+				<div className="container">
+
+
+					<div className="columns">
+						<div className="column is-two-thirds">
+		    			
+							<h2 className="title is-2">Test</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+						</div>
+
+						<div className="column">
+
+							<h3 className="title is-3">Sidebar!!!</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non</p>
+						</div>
+					</div>
+
+				</div>
+			</section>
+
+
+			<footer className="footer">
+
+				<div className="container">
+					<p>This is a footer</p>
+				</div>
+
+			</footer>
 		</div>
     );
   }
 }
+
 export default App;
